3950458438fc0399190eafec1ee213dac7f42398
BaxtiyorMurodov <baxtiyormurodovv@gmail.com>
2021-12-03 17:02:06 +0500

    misollar

--- app.js
+++ app.js
@@ -1,78 +1,69 @@
-/* 1-misol
+// 5-misol
+// let str = "samarqand toshkent";
+// let newStr = "";
 
-let i = 0
-while (i < 10) {
-    console.log(`Baxtiyor`);
-    i++
-}*/
+// for (let i = str.length - 1; i >= 0; i--) {
+//     newStr = newStr + str.charAt(i);
+// }
+// console.log(newStr);
 
-/* 2-misol
 
-let i = 0
-const myArray = [[false, true], [1, 2], 99, "yaxshi"]
-while (i < myArray.length) {
-    if (typeof(myArray[i])== 'string') {
-        console.log(i);
-    }
-    i++;
-}*/
-// let i = 0
-// let myArray = []
-// while(i<100){
-//     if (i % 3 == 0 && i % 5 == 0) {
-//       myArray.push(`FizziiiBuzz`);
-//     } else if (i % 5 == 0) {
-//       myArray.push(`Buzz`);
-//     } else if (i%3==0) {
-//       myArray.push(`fizz`);
-//     } else {
-//       myArray.push(i);
+
+// 6-misol
+// let a = prompt(`so'z kiriting`)
+// let words = a.split(' ')
+// let newWords = ''
+// let b
+// for (const word of words) {
+//    b = word.charAt().toLowerCase() + word.slice(1).toUpperCase();
+//     newWords = newWords+' '+b
+// }
+// console.log(newWords);
+
+
+       
+// 7-misol
+// let sonlar = [1,2,3,4,5,6,7,8,9,2.5,`salom`,[1,5,8]]
+// let s = 0
+// for (let i = 0; i < sonlar.length; i++) {
+//     if (typeof (sonlar[i]) == `number`) {
+//         if (sonlar[i] % 1 == 0) {
+//             s+=sonlar[i]
+//         }
+//     }   
+// }
+// console.log(s);
+
+
+
+// 8-misol
+// let a = prompt(`so'z kiriting`)
+// let b = 0
+// for (const i of a) {
+//     if (i == `a`) {
+//         b++
 //     }
-//     i++
+// }
+// console.log(b);
+
+
+
+// 9-misol
+// let myArray = [21.1, 'Roziya', "array", ["I am array"], null, true, 214];
+// for (const index in myArray) {
+//     myArray[index]=typeof(myArray[index])
 // }
 // console.log(myArray);
 
-// let myArray = [1, 2, `Ahmadjon`, null, 0,'' ,false, undefined]
-// let res = []
-// for (let i = 0; i < myArray.length; i++){
-//     if (myArray[i]) {
-//         res.push(myArray[i])
-//     }
 
+
+// 10-misol
+// let a = `salom`
+// let i = 0
+// for (const index in a) {
+//     i++
 // }
-// console.log(res);
-
-
-
-
-
-
-// 3-mosol
-let inputText = "custom string";
-let outputText = "";
-for (let i of inputText) {
-  outputText = i + outputText;
-}
-console.log(outputText); 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// console.log(i);
 
 
 
